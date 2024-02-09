25508592fbcf81df70c0726f7cd67e4f8e5397f8
mrpatrick030 <patrickmaebi@gmail.com>
2024-02-09 21:57:29 +0100

    header and footer

--- front-end-and-integration/components/footer.js
+++ front-end-and-integration/components/footer.js
@@ -17,11 +17,11 @@ export default function Footer () {
     </div>
     <div className='grid-cols-1 m-[auto]'>
       <div className='text-[120%] text-[#fff] mb-[0.2cm] font-[500]'>Join us on social media</div>
-      <img src="images/telegram.png" width="40" className='mx-[0.2cm]' style={{display:"inline-block"}} />
-      <img src="images/twitter.png" width="40" className='mx-[0.2cm]' style={{display:"inline-block"}} />
-      <img src="images/linkedin.png" width="40" className='mx-[0.2cm]' style={{display:"inline-block"}} />
-      <img src="images/youtube.png" width="40" className='mx-[0.2cm]' style={{display:"inline-block"}} />
-      <img src="images/discord.png" width="40" className='mx-[0.2cm]' style={{display:"inline-block"}} />
+      <img src="images/telegram.png" width="40" className='m-[0.2cm]' style={{display:"inline-block"}} />
+      <img src="images/twitter.png" width="40" className='m-[0.2cm]' style={{display:"inline-block"}} />
+      <img src="images/linkedin.png" width="40" className='m-[0.2cm]' style={{display:"inline-block"}} />
+      <img src="images/youtube.png" width="40" className='m-[0.2cm]' style={{display:"inline-block"}} />
+      <img src="images/discord.png" width="40" className='m-[0.2cm]' style={{display:"inline-block"}} />
     </div>
     </div>
     <div className='mb-[1cm]' style={{borderBottom:"1px solid #fff"}}></div>
