a6486341c79d612a53e6f1120d9c9264bdf634b6
mrpatrick030 <patrickmaebi@gmail.com>
2024-02-15 19:39:39 +0100

    dApp

--- front-end-and-integration/pages/dapp.js
+++ front-end-and-integration/pages/dapp.js
@@ -6,6 +6,8 @@ import 'aos/dist/aos.css';
 import axios from 'axios';
 import Header from '@/components/header';
 import { useRouter } from 'next/router';
+import { connect, disconnect } from "get-starknet";
+import { Contract, RpcProvider } from 'starknet';
 
 export default function DApp(){
   useEffect(() => {
@@ -17,12 +19,11 @@ export default function DApp(){
     bnbamount:""
 })
 
-const parsedUsdtamount = parseFloat(values.usdtamount)
-const equivUSD = isNaN(parsedUsdtamount) ? 0 : (parsedUsdtamount).toFixed(2);
-const equivBNBUSD = (isNaN(parsedUsdtamount) ? 0 : (parsedUsdtamount).toFixed(6)) - ((isNaN(parsedUsdtamount) ? 0 : (parsedUsdtamount).toFixed(6)) * 0.005);
-const equivBNB = ((equivBNBUSD)/302.2)
-const equivUSDreceived = (isNaN(parsedUsdtamount) ? 0 : (parsedUsdtamount).toFixed(2)) - ((isNaN(parsedUsdtamount) ? 0 : (parsedUsdtamount).toFixed(2)) * 0.008);
-const equivBNBreceived = (parseFloat(equivUSDreceived)/302.2).toFixed(6)
+const [amount, setamount] = useState()
+const parsedamount = parseFloat(amount)
+const equivUSD = isNaN(parsedamount) ? 0 : (parsedamount).toFixed(2);
+const equivDVS1 = isNaN(parsedamount) ? 0 : (parsedamount) * 1000;
+const equivDVS2 = isNaN(parsedamount) ? 0 : ((parsedamount) * 1000).toFixed(2);
 
 const parsedbnbamount = parseFloat(values.bnbamount)
 const equivBNBtoUSD = (isNaN(parsedbnbamount) ? 0 : (((parsedbnbamount).toFixed(2)) * 302.2));
@@ -33,22 +34,6 @@ const equivUSDreceived4 = (equivBNBtoUSD - (equivBNBtoUSD * 0.00698)).toFixed(2)
 const providerfee1 = (equivBNBtoUSD * 0.00684).toFixed(6)
 const providerfee2 = (equivBNBtoUSD * 0.00685).toFixed(2)
 
-const handleChange = (event) => {
-    const { name, value } = event.target;
-    setValues({
-      ...values,
-      [name]: value,
-    });
-  };
-
-  const ApproveError = async () => {
-   document.getElementById("approveerrordiv").innerHTML = "Insufficient BNB for transaction fees!"
-  }
-
-  const PreviewError = async () => {
-    document.getElementById("previewerrordiv").innerHTML = "You do not have enough BNB to swap!"
-   }
-
   const [swapOption, setSwapOption] = useState(true)
   const changeSwapOption = () => {
     setSwapOption(false);
@@ -56,6 +41,66 @@ const handleChange = (event) => {
   const changeSwapOptionBack = () => {
     setSwapOption(true);
   }
+
+
+  // wallet connection instructions
+  const [connectWallet, setConnectWallet] = useState(true)
+  const [connectedWallet, setConnectedWallet] = useState(false)
+  const [theWalletAddress, setTheWalletaddress] = useState()
+  //to connect our wallet
+  //we will use cookies to ensure consistency of the connecting button on all pages of the application
+  const cookie = require('cookie');
+  const connecttheWallet = async (e) => {
+    try {
+      e.preventDefault();
+      const starknet = await connect();
+      const walletAddress = starknet.account.address
+      console.log(walletAddress)
+      setTheWalletaddress(walletAddress)
+      
+       // Update state
+       setConnectedWallet(true);
+       setConnectWallet(false);  
+
+     // Set the wallet address as a cookie on the browser
+     document.cookie = `walletaddresscookie=${encodeURIComponent(walletAddress)}; secure; max-age=${60 * 60 * 24}; sameSite=strict; path=/`;
+    } catch (error) {
+      console.log('Error connecting to StarkNet:', error.message);
+    }
+  };
+
+  // this ensures continous mounting of the connecting button on all pages and it runs once using the useEffect hook
+ const confirmCookies = () => {
+  // to check if the wallet address cookie is present
+  const walletCookie = document.cookie
+    .split(';')
+    .find((cookie) => cookie.trim().startsWith('walletaddresscookie='));
+
+  if (walletCookie) {
+    // Extracting the wallet address from the cookie
+    const walletAddress = decodeURIComponent(walletCookie.split('=')[1]);
+
+    // Update state
+    setConnectedWallet(true);
+    setConnectWallet(false);
+    setTheWalletaddress(walletAddress);
+  }
+};
+
+useEffect(() => {
+  confirmCookies();
+}, []); 
+
+
+  //to disconnect our wallet
+  const disconnectWallet = async(e) => {
+    e.preventDefault();
+    const starknet = await disconnect();
+    setConnectedWallet(false)
+    setConnectWallet(true)
+      // Delete the wallet address cookie
+    document.cookie = 'walletaddresscookie=; max-age=0; path=/';
+  } 
 
   return (
     <>
@@ -75,76 +120,57 @@ const handleChange = (event) => {
 <div className='lg:px-[30%] md:px-[25%] p-[5%]'>
 {swapOption ? 
 (<div className='bg-[#111] p-[1cm] rounded-xl dAppdivs'>
-<div className='p-[0.5cm] pb-[1cm] bg-[#eee] rounded-xl'>
-<div className='text-[#555] font-[500]'><span>You Pay</span><span className='float-right text-[#02f]'>Max</span></div>
+<div className='p-[0.5cm] pb-[1.5cm] bg-[#eee] rounded-xl'>
+<div className='text-[#333] font-[500]'><span>You Pay</span><span className='float-right text-right text-[#003]'>Amount</span></div>
 <div className='mt-[0.5cm] clear-both' style={{display:"block"}}>
-    <div style={{display:"inline-block"}}><img src="images/tether.png" width="40" /></div>
-    <div className='ml-[0.5cm]' style={{display:"inline-block"}}><span>USDT</span><br /><span className='text-[80%] text-[#555]'>BNB Smart Chain</span></div>
-    <input style={{display:"inline-block"}} className="font-[600] w-[30%] float-right text-[150%] text-right bg-[#eee] outline-none text-[#000] placeholder-[#000]" type="text" id="usdtamount" name="usdtamount" value={values.usdtamount} onChange={handleChange} placeholder='0' />
+  <select className='text-[#222] outline-none' style={{textDecorationColor:"none"}}>
+    <option>USDT</option>
+    <option>DAI</option>
+    <option>USDC</option>
+  </select>
+    <input style={{display:"inline-block"}} className="font-[600] w-[30%] float-right text-[150%] text-right bg-[#eee] outline-none text-[#000] placeholder-[#000]" type="text" id="amount" name="amount" onChange={(e) => setamount(e.target.value)} placeholder='0' />
 </div>
-<div className='float-right text-[#555]'>≈${equivUSD}</div>
+<div className='float-right clear-both text-[#555]'>≈${Intl.NumberFormat().format(equivUSD)}</div>
 </div>
 
-<div className='text-center text-[120%] my-[0.1cm]'><i className='fa fa-exchange swapbutton rounded-full bg-[#eee] text-[#555] p-[0.3cm] cursor-pointer' onClick={(e) => changeSwapOption()}></i></div>
+<div className='text-center text-[120%] my-[0.1cm]'><i className='fa fa-exchange switchbutton rounded-full bg-[#502] text-[#eee] p-[0.3cm] cursor-pointer font-[600]' onClick={(e) => changeSwapOption()}></i></div>
 
 <div className='p-[0.5cm] pb-[1cm] bg-[#eee] rounded-xl'>
-<div className='text-[#555] font-[500]'><span>You Get</span></div>
+<div className='text-[#333] font-[500]'><span>You Get</span></div>
 <div className='mt-[0.5cm]' style={{display:"block"}}>
-    <img src="images/bnb.png" width="40" className='rounded-full bg-[#ccc]' style={{display:"inline-block"}} />
-    <div className='ml-[0.5cm]' style={{display:"inline-block"}}><span>BNB</span></div>
-    <input style={{display:"inline-block"}} className="font-[600] w-[30%] float-right text-[150%] text-right bg-[#eee] outline-none text-[#000]" value={equivBNB} type="text" placeholder='0' />
+    <img src="images/tokens.png" width="40" style={{display:"inline-block"}} />
+    <div className='ml-[0.5cm]' style={{display:"inline-block"}}><span className='text-[#222]'>DVS</span></div>
+    <input style={{display:"inline-block"}} className="font-[600] w-[30%] float-right text-[150%] text-right bg-[#eee] outline-none text-[#000]" value={Intl.NumberFormat().format(equivDVS1)} type="text" placeholder='0' />
 </div>
-<div className='float-right text-[#555]'>≈${equivBNBUSD}</div>
+<div className='float-right text-[#555]'>≈${Intl.NumberFormat().format(equivDVS2)}</div>
 </div>
 
-<div className='my-[0.5cm] text-center text-[#555] text-[110%] font-[500]'>1 BNB ≈ 302.265312 USDT &nbsp; <i className='fa fa-exchange text-[#555]'></i></div>
+<div className='my-[0.5cm] text-center text-[#555] text-[110%] font-[500]'>$1 ≈ 1000 DVS &nbsp; <i className='fa fa-exchange text-[#555]'></i></div>
 
-<div className='text-[#e00] font-[500] mb-[1cm] text-center' id="approveerrordiv" style={{transition:"0.5s ease-in-out"}}></div>
+<div><button className='text-center py-[0.3cm] bg-[#502] font-[500] text-[#fff] w-[100%] rounded-full generalbutton2 cursor-pointer' style={{display:"inline-block"}}>Swap</button></div>
 
-<div><span className='text-center py-[0.3cm] bg-[#02f] font-[500] text-[#fff] w-[48%] rounded-full restorebutton cursor-pointer' style={{display:"inline-block"}} onClick={(e) => ApproveError()}>Approve</span><span className='text-center font-[500] w-[48%] py-[0.3cm] bg-[#eee] text-[#555] rounded-full float-right' style={{display:"inline-block"}} >Preview Swap</span></div>
-
-<div className='my-[1cm] text-center text-[#555] text-[110%] font-[500]'>Approve USDT before you can swap it &nbsp; <i className='fa fa-info-circle'></i></div>
+<div className='my-[1cm] text-center text-[#aaa] text-[110%] font-[500]'>Approve token before you can swap it &nbsp; <i className='fa fa-info-circle'></i></div>
 
 <div className='p-[0.5cm] bg-[#eee] rounded-md mb-[4cm]'>
-  <div className='mb-[0.5cm]'><span className='text-[#555]'>Minimum Received</span><span className='text-[#000] float-right'>{equivBNBreceived} BNB (≈${equivUSDreceived})</span></div>
-  <div><span className='text-[#555]'>Slippage Tolerance</span><span className='text-[#000] float-right'>1.0 &nbsp; <i className='fa fa-pencil text-[#555]'></i></span></div>
+  <div className='mb-[0.5cm]'><span className='text-[#555]'>Minimum Received</span><span className='text-[#000] float-right'>{equivDVS2} DVS (≈${equivUSD})</span></div>
+  <div className='mb-[0.5cm]'><span className='text-[#555]'>Provider Fee &nbsp; <i className='fa fa-info-circle'></i></span><span className='text-[#000] float-right'>${providerfee1} (≈${providerfee2})</span></div>
 </div>
 </div>)
   : 
 (<div className='bg-[#111] p-[1cm] rounded-xl dAppdivs'>
 <div className='p-[0.5cm] pb-[1cm] bg-[#eee] rounded-xl'>
-<div className='text-[#555] font-[500]'><span>You Pay</span><span className='float-right text-[#02f]'>Max</span></div>
+<div className='text-[#333] font-[500]'><span>You Pay</span><span className='float-right text-right text-[#003]'>Amount</span></div>
 <div className='mt-[0.5cm] clear-both' style={{display:"block"}}>
-    <img src="images/bnb.png" width="40" style={{display:"inline-block"}} />
-    <div className='ml-[0.5cm]' style={{display:"inline-block"}}><span>BNB</span></div>
-    <input style={{display:"inline-block"}} className="font-[600] w-[30%] placeholder-[#000] float-right text-[150%] text-right bg-[#eee] outline-none text-[#000]" type="text" id="bnbamount" name="bnbamount" value={values.bnbamount} onChange={handleChange} placeholder='0' />
+    <div style={{display:"inline-block"}}><img src="images/ether.png" width="40" /></div>
+    <div className='ml-[0.5cm]' style={{display:"inline-block"}}><span className='text-[#222]'>ETH</span><br /><span className='text-[80%] text-[#502]'>StarkNet</span></div>
+    <input style={{display:"inline-block"}} className="font-[600] w-[30%] float-right text-[150%] text-right bg-[#eee] outline-none text-[#000] placeholder-[#000]" type="text" id="amount" name="amount" onChange={(e) => setamount(e.target.value)} placeholder='0' />
 </div>
-<div className='float-right text-[#555]'>≈${equivBNBtoUSD}</div>
-</div>
-
-<div className='text-center text-[120%] my-[0.1cm]'><i className='fa fa-exchange swapbutton text-[#555] rounded-full bg-[#eee] p-[0.3cm] cursor-pointer' onClick={(e) => changeSwapOptionBack()}></i></div>
-
-<div className='p-[0.5cm] pb-[1cm] bg-[#eee] rounded-xl'>
-<div className='text-[#555] font-[500]'><span>You Get</span></div>
-<div className='mt-[0.5cm]' style={{display:"block"}}>
-<div style={{display:"inline-block"}}><img src="images/tether.png" width="40" className='rounded-full bg-[#ccc]' /></div>
-    <div className='ml-[0.5cm]' style={{display:"inline-block"}}><span>USDT</span><br /><span className='text-[80%] text-[#555]'>BNB Smart Chain</span></div>
-    <input style={{display:"inline-block"}} className="font-[600] w-[30%] float-right text-[150%] text-right bg-[#eee] outline-none text-[#000]" value={equivUSDBNB} type="text" placeholder='0' />
-</div>
-<div className='float-right text-[#555]'>≈${equivUSDBNB2}</div>
 </div>
 
-<div className='my-[1cm] text-center text-[#555] text-[110%] font-[500]'>1 BNB ≈ 302.265312 USDT &nbsp; <i className='fa fa-exchange text-[#555]'></i></div>
+<div className='text-center text-[120%] my-[0.1cm]'><i className='fa fa-exchange switchbutton rounded-full bg-[#eee] text-[#502] p-[0.3cm] cursor-pointer font-[600]' onClick={(e) => changeSwapOptionBack()}></i></div>
 
-<div className='text-[#e00] font-[500] mb-[1cm] text-center' id="previewerrordiv" style={{transition:"0.5s ease-in-out"}}></div>
+<div><button className='text-center py-[0.3cm] bg-[#502] font-[500] text-[#fff] w-[100%] rounded-full generalbutton2 cursor-pointer' style={{display:"inline-block"}}>Swap</button></div>
 
-<div className='text-center py-[0.3cm] bg-[#02f] font-[500] text-[#fff] rounded-full restorebutton cursor-pointer mb-[1cm]' onClick={(e) => PreviewError()}>Preview Swap</div>
-
-<div className='p-[0.7cm] bg-[#eee] rounded-md mb-[4cm]'>
-  <div className='mb-[0.5cm]'><span className='text-[#555]'>Minimum Received</span><span className='text-[#000] float-right'>{equivUSDreceived3} USDT (≈${equivUSDreceived4})</span></div>
-  <div className='mb-[0.5cm]'><span className='text-[#555]'>Slippage Tolerance</span><span className='text-[#000] float-right'>1.0 &nbsp; <i className='fa fa-pencil text-[#555]'></i></span></div>
-  <div><span className='text-[#555]'>Provider Fee &nbsp; <i className='fa fa-info-circle'></i></span><span className='text-[#000] float-right'>{providerfee1} USDT (≈${providerfee2})</span></div>
-</div>
 </div>)
 }
 
